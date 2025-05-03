a8783c6266a4b9bc2b6d345539a30880249fe4e5
Aaron Lesbão Dumont <aarondmt@gmail.com>
2025-05-03 10:37:39 -0400

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="meu_investimento-package",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     description="Pacote para calcular o retorno de investimento em ações.",
     author="Aaron Lesbão Dumont",
